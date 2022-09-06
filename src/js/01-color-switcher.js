762d9d0def1e5cd9c6f938b827e48bf13514e304
Vitaliy Mishugli <vmishughli@gmail.com>
2022-09-06 18:59:06 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let intervalId = null;
+
+refs.start.addEventListener('click', () => {
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+  intervalId = setInterval(() => {
+    let color = getRandomHexColor();
+    refs.body.style.backgroundColor = color;
+  }, 1000);
+});
+
+refs.stop.addEventListener('click', () => {
+  clearInterval(intervalId);
+  refs.body.style.backgroundColor = '#fff';
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
