7c95e4517f0eba3059d5e87363e3ba546d005ef3
Vitaliy Mishugli <vmishughli@gmail.com>
2022-09-09 22:12:58 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -16,25 +16,28 @@ refs.formEl.addEventListener('submit', (e) => {
     // console.log(Number(delay.value), Number(step.value), Number(amount.value));
   
   for (let i = 0; i < amount; i = i + 1){
-    // console.log(i);
-    delay = delay + step;
     createPromise(i, delay)
-      .then(({ position, delay }) => {
-        setTimeout(() => {
-          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        }, delay)
-      })
-      .catch(({ position, delay }) => {
-        setTimeout(() => {
-          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        }, delay)        
-      });
+    .then(({ position, delay }) => {
+      setTimeout(() => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      }, delay);
+    })
+    .catch(({ position, delay }) => {
+      setTimeout(() => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      }, delay);
+    });
+    delay = delay + step;
   }
    
 })
 
 function createPromise(position, delay) {
-  
+
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
